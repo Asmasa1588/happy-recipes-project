c74931ad21cb6dc3c26698e9757fa599379fb2a0
Asma <as1588ma@gmail.com>
2022-05-06 21:31:43 -0400

    implement confirm legalage functionality

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-console.log("hello world")
-const init = ()=>{
-    
-}
+console.log("hello world");
+const init = () => {
+  const isLegalAge = confirm("Are you 21 years or older?");
+  if (isLegalAge) {
+    main();
+  } else {
+    const app = document.getElementById("app");
+    app.innerHTML = "We are sorry, but you are not old enough!";
+  }
+};
+const main = () => {
+  //The core of the project
+};
 
 document.addEventListener("DOMContentLoaded", init);
