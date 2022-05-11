165751e1395033387e64207520d35cc7bf6d7f88
Asma <as1588ma@gmail.com>
2022-05-10 21:47:26 -0400

    implement filter functionality

--- index.js
+++ index.js
@@ -8,21 +8,39 @@ const init = () => {
     app.innerHTML = "We are sorry, but you are not old enough!";
   }
 };
+
 const main = () => {
   //The core of the project
   const recipesContainer = document.getElementById("recipes-container");
-  fetch("http://localhost:3000/recipes")
-    .then((res) => res.json())
-    .then((data) => {
-      console.log({ data });
-      data.forEach((recipe) => {
-        const liElement = document.createElement("li");
-        liElement.innerHTML = recipe.title;
-        recipesContainer.appendChild(liElement);
+  const renderRecipe = (recipe, filterText) => {
+    const shouldRenderRecipe =
+      (filterText && recipe.title.includes(filterText)) || !filterText;
+    if (shouldRenderRecipe) {
+      const liElement = document.createElement("li");
+      liElement.innerHTML = recipe.title;
+      recipesContainer.appendChild(liElement);
+    }
+  };
+  const fetchBy = (searchTerm) => {
+    fetch("http://localhost:3000/recipes")
+      .then((res) => res.json())
+      .then((data) => {
+        recipesContainer.innerHTML = "";
+        data.forEach((recipe) => {
+          if (searchTerm) {
+            renderRecipe(recipe, searchTerm);
+          } else {
+            renderRecipe(recipe);
+          }
+        });
       });
-    });
-
-  document.getElementById("recipe-filter");
+  };
+  fetchBy();
+  const recipeFilter = document.getElementById("recipe-filter");
+  recipeFilter.addEventListener("change", (event) => {
+    console.log({ event: event.target.value });
+    fetchBy(event.target.value);
+  });
 };
 
 document.addEventListener("DOMContentLoaded", init);
